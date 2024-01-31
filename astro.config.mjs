84adb2bd9cd46f719b7b5460b553bebff9d7c251
Stefano Garavelli <1073725+killer1loop@users.noreply.github.com>
2024-01-31 14:12:46 +0100

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,12 @@ import { defineConfig } from 'astro/config';
 import cloudflare from "@astrojs/cloudflare";
 import tailwind from "@astrojs/tailwind";
 import markdoc from "@astrojs/markdoc";
-
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
   adapter: cloudflare(),
+  site: 'https://garavelli.io',
   integrations: [tailwind(), markdoc(), sitemap()]
 });
