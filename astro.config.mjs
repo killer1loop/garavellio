9595072129e01e73639ad276a2b92b2634cd1554
killer loop <1073725+killer1loop@users.noreply.github.com>
2024-09-26 14:10:26 +0200

    netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,17 @@
 import { defineConfig } from 'astro/config';
-import cloudflare from "@astrojs/cloudflare";
 import tailwind from "@astrojs/tailwind";
 import markdoc from "@astrojs/markdoc";
 import sitemap from "@astrojs/sitemap";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: cloudflare(),
+  adapter: netlify(),
+  image: {
+    domains: ['directus.backend.garavelli.io'],
+  },
   site: 'https://garavelli.io',
   integrations: [tailwind(), markdoc(), sitemap()]
 });
