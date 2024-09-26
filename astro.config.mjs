47d3e24612f809b7260dbf0c759143dcc94dbef0
killer loop <1073725+killer1loop@users.noreply.github.com>
2024-09-26 13:23:08 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,9 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: cloudflare(),
+  adapter: cloudflare({
+    imageService: 'cloudflare'
+}),
   site: 'https://garavelli.io',
   integrations: [tailwind(), markdoc(), sitemap()]
 });
